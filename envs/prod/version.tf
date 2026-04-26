a07ba30bf86424c971cb1799cdcfe96603a3a5cf
Edwin-Oghenetejiri1 <darealthej@gmail.com>
2026-04-26 21:07:15 +0100

    new repo

--- envs/prod/version.tf
+++ envs/prod/version.tf
@@ -1,4 +1,5 @@
 terraform {
+  # 1. Backend Configuration
   backend "s3" {
     bucket         = "retrogame-tfstate-573986291693"
     key            = "prod/eks/terraform.tfstate"
@@ -9,6 +10,8 @@ terraform {
 
   required_version = ">= 1.0"
 
+  # 2. Explicit Provider Sources
+  # This section stops the "hashicorp/kubectl" not found error
   required_providers {
     aws = {
       source  = "hashicorp/aws"
@@ -18,9 +21,22 @@ terraform {
       source  = "hashicorp/tls"
       version = "~> 4.0"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.14.0"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.0"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0"
+    }
   }
 }
 
+# 3. Provider Configurations
 provider "aws" {
   region = var.region
 }
@@ -28,6 +44,42 @@ provider "aws" {
 provider "aws" {
   alias  = "virginia"
   region = "us-east-1"
+}
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  load_config_file       = false
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
+    }
+  }
 }
 
 data "aws_region" "current" {}
